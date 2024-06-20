9e1a19f14ca51c5aae52c0a674b7d6e732238d54
Nik Holo <nikita@golodenko.com>
2024-06-20 19:34:18 +0200

    add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_160449) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_171955) do
   create_table "links", force: :cascade do |t|
     t.string "code"
     t.string "redirect_url"
     t.integer "counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_links_on_code", unique: true
   end
 
 end
